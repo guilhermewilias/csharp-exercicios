f30a92bf6dc0d9ae3a2720db1901ee944a07b38d
Guilherme Wilias <113528431+guilhermewilias@users.noreply.github.com>
2023-11-10 11:51:38 -0300

    Add files via upload

---
+++ Program-Ex5.cs
@@ -0,0 +1,23 @@
+﻿/*
+
+Ler uma temperatura em graus Celsius e apresentá-Ia convertida em graus Fahrenheit. 
+A fórmula de conversão de temperatura a ser utilizada é F = (9 * C + 160) / 5. 
+
+Em que a variável F representa é a temperatura em graus Fahrenheit e a variável C representa é a temperatura em graus Celsius. 
+
+*/
+
+
+static void main()
+{
+    Console.Write("Escreva uma temperatura em graus Celsius --> ");
+    string c = Console.ReadLine();
+    double cNumber = double.Parse(c);
+
+    double f = (9 * cNumber + 160) / 5;
+
+    Console.WriteLine($"\n\nA temperatura em celsius foi {cNumber}º");
+    Console.WriteLine($"A temperatura em fahrenheit foi {f}");
+}
+main();
+
---
+++ ProgramEx6.cs
@@ -0,0 +1,23 @@
+﻿/*
+
+Ler uma temperatura em graus Fahrenheit e apresentá-Ia convertida em graus Celsius.
+
+A fórmula de conversão de temperatura a ser utilizada é C = (F - 32) * 5 / 9. 
+
+Em que a variável F é a temperatura em graus Fahrenheit e a variável C é a temperatura em graus Celsius.
+
+*/
+
+static void main()
+{
+    Console.Write("Escreva uma temperatura em Fahrenheit --> ");
+    string f = Console.ReadLine();
+    double fNumber = double.Parse(f);
+
+    double c = (fNumber - 32) * 5 / 9;
+
+    Console.WriteLine($"\n\nA temperatura em Fahrenheit é {fNumber}");
+    Console.WriteLine($"A temperatura em graus Celsius é {c}º");
+}
+
+main();
